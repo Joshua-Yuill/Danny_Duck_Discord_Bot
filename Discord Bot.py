34ca043b0f6ad543608fa63720d7d64015cf4176
Joshua <78020975+Joshua-Yuill@users.noreply.github.com>
2022-11-20 22:19:29 +0000

    Initial commit

---
+++ Discord Bot.py
@@ -0,0 +1,38 @@
+import discord
+from dadjokes import Dadjoke
+import randfacts
+
+
+ 
+intents = discord.Intents.all()
+client = discord.Client(command_prefix='/', intents=intents)
+ 
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+ 
+count = 0
+
+@client.event
+async def on_message(message):
+    print("message-->", message)
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith('hi'):
+        await message.channel.send("Hello!")
+ 
+    if message.content.startswith('dad' or 'Dad'):
+        dadjoke = Dadjoke()
+        await message.channel.send(dadjoke.joke)
+        
+    if message.content.startswith('quack' or 'Quack'):
+        await message.channel.send("Quack Quack!")
+        
+    if message.content.startswith('fact' or 'Fact'):
+        fact = randfacts.get_fact()
+        await message.channel.send(fact) 
+        
+        
+ 
+client.run('NDMwNzY0MDk0Mjg3NzczNjk2.GRDsGH.MBKyVQ070CZT8GGoALhj9E01yBb-bazh2PpDh4')
